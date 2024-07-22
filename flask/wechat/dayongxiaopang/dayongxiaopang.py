32fed946f821b575bed0ff18b238deb4d8fec86c
tangyue <yuetang1024@163.com>
2024-07-23 01:54:43 +0800

    [update]增加获取目录功能和获取书本内容功能。

--- flask/wechat/dayongxiaopang/dayongxiaopang.py
+++ flask/wechat/dayongxiaopang/dayongxiaopang.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response  
+import json
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
@@ -41,10 +42,50 @@ def shiyi():
             retStr += p.text.strip()
     else:
         print(f"Failed to retrieve the webpage: {response.status_code}")
+        retStr = "获取释义失败。"
 
     print(retStr)
 
     return retStr
+
+# 获取二十四史目录
+# 读取 JSON 文件
+def load_json_data(filename):
+    with open("24book/" + filename, 'r') as file:
+        data = json.load(file)
+    return data
+
+# 加载 JSON 数据
+data = load_json_data('data.json')
+
+@app.route("/catalogue",methods=['GET'])
+def getCatalogue():
+    print("enter getCatalogue")
+    return jsonify(data)
+
+@app.route("/book",methods=['GET'])
+def getBook():
+    book = request.args.get('book')  # 获取参数 book 的值
+    chapter = request.args.get('chapter')  # 获取参数 book 的值
+
+    file_path = '24book/'
+    file_path += book
+    file_path += '/'
+    file_path += chapter
+    file_path += '.txt'
+
+    print(file_path)
+  
+    try:  
+        # 以只读模式打开文件  
+        with open(file_path, 'r', encoding='utf-8') as file:  
+            # 读取文件全部内容  
+            content = file.read()  
+            # 返回内容，这里使用Response对象，你可以根据需要设置MIME类型  
+            return Response(content, mimetype='text/plain')  
+    except FileNotFoundError:  
+        # 如果文件不存在，返回错误信息  
+        return "File not found", 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port =8080)
